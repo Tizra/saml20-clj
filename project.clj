3ab8476985ee897c159cc1c1a91d5f4a33794e23
David G. Durand <david.durand@tizra.com>
2020-01-08 16:01:29 -0500

    Try again to get jitpack to work

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject saml20-clj "0.1.6-tizra.2"
+(defproject saml20-clj "0.1.6-tizra.3"
   :description "Basic SAML 2.0 library for SSO."
-  :url "https://github.com/vlacs/saml20-clj"
+  :url "https://github.com/Tizra/saml20-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
@@ -14,7 +14,8 @@
                  [org.clojure/data.zip "0.1.3"]
                  ]
   :pedantic :warn
-  :profiles {:dev {:source-paths ["dev" "test"]
+  :profiles {:dev {}
+             :test {:source-paths ["dev" "test"]
                    :dependencies [[org.clojure/tools.nrepl "0.2.3"]
                                   [org.vlacs/helmsman "1.0.0-alpha5"]
                                   [hiccup "1.0.5"]
